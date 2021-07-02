c59eca5efbc6cd5685deed9cd3ebf89e2edf7901
Portgas D Ace 667 <71558729+SinX28@users.noreply.github.com>
2021-07-02 14:32:29 +0200

    Delete save_reader.py

--- save_reader.py
+++
@@ -1,8 +0,0 @@
-import pickle
-
-print("   Lecture de la Save...\n")
-
-data = pickle.load( open( "scrap_conf.p", "rb" ),encoding='bytes' )
-
-print("query: "+data['query']+"\nchemin: "+data['result_path']+"\nnavigateur: "+data['browser']+"\n")
-
